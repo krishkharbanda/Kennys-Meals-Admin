30b4dc0de27c09e915bfb23c22df218b0199e55d
Krish Kharbanda <kharbandakrish23@gmail.com>
2024-06-16 20:30:20 -0400

    Initial Commit

---
+++ Kennys Meals Admin/Kennys_Meals_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kennys_Meals_AdminApp.swift
+//  Kennys Meals Admin
+//
+//  Created by Krish on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Kennys_Meals_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kennys Meals AdminUITests/Kennys_Meals_AdminUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Kennys_Meals_AdminUITestsLaunchTests.swift
+//  Kennys Meals AdminUITests
+//
+//  Created by Krish on 6/16/24.
+//
+
+import XCTest
+
+final class Kennys_Meals_AdminUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
